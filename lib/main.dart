bd8c53861db0fbc0bad3832fff985875589f5172
yunajung224 <qpla2424@gmail.com>
2025-12-23 17:30:37 +0900

    aindocs: 메인 파일 추가 설명 완료

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Nanum_Gothic',
+        fontFamily: 'Nanum_Gothic', //앱 전체에 사용할 기본 폰트 설정
         useMaterial3: true,
         scaffoldBackgroundColor: Colors.grey[400],
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
+          backgroundColor: Colors.white, //라이트 모드 AppBar 배경색
+          foregroundColor: Colors.black, //AppBar안의 텍스트/아이콘 색상
         ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.lightGreen,
-          brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed( 
+          seedColor: Colors.lightGreen, //앱의 기본 컬러 설정
+          brightness: Brightness.light, //라이트 모드 색상 스킴 사용
         ),
       ),
-      darkTheme: ThemeData(
+      darkTheme: ThemeData( //다크 모드 시 적용될 테마
         useMaterial3: true,
         brightness: Brightness.dark,
         appBarTheme: const AppBarTheme(
-        foregroundColor: Colors.white,
+        foregroundColor: Colors.white, //다크 모드 AppBar 텍스트 컬러 화이트로 지정
         ),
 
           colorScheme: ColorScheme.dark(
